5588e5efa10e2adde559bc2ad7c6771532588c30
Suraj hinge <surajhinge07@gmail.com>
2024-12-20 15:41:08 +0530

    Moved index.html at correct location

--- product_costing/product_costing/settings.py
+++ product_costing/product_costing/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'product_costing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
